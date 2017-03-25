5d730149ef649c0a6e4f19f0cd0d0e922ab3b610
Sam Light <samlight1994@gmail.com>
2017-03-25 16:44:11 +0000

    Collition detection.

--- src/tetris/grid.cljs
+++ src/tetris/grid.cljs
@@ -18,32 +18,100 @@
   (>= (+ (shapes/height @current-shape) (:pos-y @current-shape))
      height))
 
+(defn any-kv [pred col]
+  (let [len (count col)]
+    (loop [i 0]
+      (if (pred i (get col i))
+        true
+        (if (< i (dec len))
+          (recur (inc i))
+          false)))))
+
+(defn check-rows [ox cxl crow sxl srow]
+  (any-kv
+   (fn [ci cc]
+      (any-kv
+       (fn [si sc]
+         (and (pos? cc) (pos? sc) (= (+ cxl ci ox) (+ sxl si))))
+       srow))
+   crow))
+
+(defn check-shape-in-box [ox oy cshp sshp]
+  (let [cpos-xl (:pos-x cshp)
+        cpos-yt (:pos-y cshp)
+        spos-xl (:pos-x sshp)
+        spos-yt (:pos-y sshp)]
+    (any-kv
+     (fn [cr-y crow]
+       (let [crpos-y (+ cpos-yt cr-y)]
+         (any-kv
+          (fn [sr-y srow]
+            (and (= (+ crpos-y oy) (+ sr-y spos-yt))
+                 (check-rows ox cpos-xl crow spos-xl srow)))
+          (:cords sshp))))
+     (:cords cshp))))
+
+(defn check-collision [ox oy]
+  (let [cshp @current-shape
+        cheight (shapes/height cshp)
+        cwidth (shapes/width cshp)
+        cpos-xl (:pos-x cshp)
+        cpos-xr (+ cpos-xl (dec cwidth))
+        cpos-yt (:pos-y cshp)
+        cpos-yb (+ cpos-yt (dec cheight))]
+    (some
+     #(let [swidth (shapes/width %)
+            sheight (shapes/height %)
+            spos-xl (:pos-x %)
+            spos-xr (+ spos-xl (dec swidth))
+            spos-yt (:pos-y %)
+            spos-yb (+ spos-yt (dec sheight))
+            top-r (-> % :cords first)]
+        (if (and (>= (+ cpos-yb oy) spos-yt)
+                 (<= (+ cpos-yt oy) spos-yb)
+                 (>= (+ cpos-xr ox) spos-xl)
+                 (<= (+ cpos-xl ox) spos-xr))
+          (check-shape-in-box ox oy cshp %)
+          false))
+     @shapes)))
+
 (defn place []
-  (if (or (check-bottom))
+  (if (or (check-bottom)
+          (check-collision 0 1))
     (do (swap! shapes conj @current-shape)
         (reset! current-shape nil)
         true)
     false))
 
 (defn move-shape-left []
-  (when (> (:pos-x @current-shape) 0)
+  (when (and (> (:pos-x @current-shape) 0)
+             (not (check-collision -1 0)))
     (swap! current-shape #(assoc-in % [:pos-x] (-> % :pos-x dec)))))
 
 (defn move-shape-right []
-  (when (< (:pos-x @current-shape) (- width (shapes/width @current-shape)))
+  (when (and (< (:pos-x @current-shape) (- width (shapes/width @current-shape)))
+             (not (check-collision 1 0)))
     (swap! current-shape #(assoc-in % [:pos-x] (-> % :pos-x inc)))))
 
 (defn move-shape-down []
-  (swap! current-shape #(assoc-in % [:pos-y] (-> % :pos-y inc))))
+  (when-not (place) (swap! current-shape #(assoc-in % [:pos-y] (-> % :pos-y inc)))))
+
+(defn rotate-shape []
+  (shapes/rotate current-shape)
+  (when (> (+ (:pos-x @current-shape) (shapes/width @current-shape)) width)
+    (swap! current-shape
+           #(assoc-in % [:pos-x] (- width (shapes/width @current-shape))))))
 
 (defn move-current-shape []
-  (when (and (time/check-move)
-             (not (place)))
+  (when (time/check-move)
     (move-shape-down)
     (time/set-move)))
 
 (defn make-shape [shape]
   (assoc shape
+         :opposite false
+         :flip-x false
+         :flip-y false
          :pos-x (- (/ width 2)
                    (.round js/Math (-> shape :cords first count (/ 2))))
          :pos-y (-> shape :cords count - (+ 1))))
@@ -52,16 +120,20 @@
   (reset! current-shape (make-shape (shapes/pick-shape)))
   (time/set-move))
 
-(def control-left  (atom false))
-(def control-right (atom false))
+(def control-left   (atom false))
+(def control-right  (atom false))
+(def control-down   (atom false))
+(def control-rotate (atom false))
 
 (defn control-toggling [con done fun]
   (when (and @con (not @done)) (fun) (reset! done true))
   (when (and (not @con) @done) (reset! done false)))
 
 (defn control-shape []
-  (control-toggling control/left-active control-left move-shape-left)
-  (control-toggling control/right-active control-right move-shape-right))
+  (control-toggling control/left-active  control-left   move-shape-left)
+  (control-toggling control/right-active control-right  move-shape-right)
+  (control-toggling control/down-active  control-down   move-shape-down)
+  (control-toggling control/up-active    control-rotate rotate-shape))
 
 (defn render-shape [shape]
   (let [cords (:cords shape)
