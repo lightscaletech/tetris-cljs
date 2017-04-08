66f40bd0c5d06f0b3e87e157e8cd0177e447384f
Sam Light <samlight1994@gmail.com>
2017-04-08 09:55:13 +0100

    Created a countdown animation.

---
+++ src/tetris/countdown.cljs
@@ -0,0 +1,60 @@
+(ns tetris.countdown
+  (:require [tetris.time :as time]
+            [tetris.canvas :as can]
+            [tetris.layout :as lo]))
+
+(def nums {3 {:cords [[0 1 1 0]
+                      [1 0 0 1]
+                      [0 0 1 0]
+                      [1 0 0 1]
+                      [0 1 1 0]]}
+           2 {:cords [[0 1 1 0]
+                      [1 0 0 1]
+                      [0 0 0 1]
+                      [0 0 1 0]
+                      [1 1 1 1]]}
+           1 {:cords [[0 0 1 0]
+                      [0 1 1 0]
+                      [0 0 1 0]
+                      [0 0 1 0]
+                      [0 1 1 1]]}})
+
+(def interval 1000)
+(def change-time (atom nil))
+(def current (atom nil))
+
+(defn init []
+  (reset! change-time (time/now))
+  (reset! current 3))
+
+(defn deinit []
+  (reset! change-time nil)
+  (reset! current nil))
+
+(defn change []
+  (reset! change-time (time/now))
+  (swap! current dec))
+
+(defn last? [] (= @current 1))
+
+(defn timeout? [] (> (- (time/now) @change-time) interval))
+
+(defn render-num [{:keys [cords]}]
+  (let [gx (lo/cgrid-x) gy (lo/cgrid-y)
+        mp! #(+ %1 (* @lo/ssize (+ %2 %3)))]
+      (doseq [[r cols] (map-indexed vector cords)]
+        (doseq [[c v] (map-indexed vector cols)]
+          (when (pos? v)
+            (can/draw-rectangle
+             (mp! gx 3 c) (mp! gy 7 r)
+             @lo/ssize @lo/ssize
+             "#FFF"))))))
+
+(defn render []
+  (when (and (nil? @change-time) (nil? @current)) (init))
+  (render-num (get nums @current))
+  (if (timeout?)
+    (if (last?)
+      (do (deinit) false)
+      (do (change) true))
+    true))
