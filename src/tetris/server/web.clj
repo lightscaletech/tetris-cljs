6d97e8c7129f45802085683b6da6ab25f2e481b2
Sam Light <samlight1994@gmail.com>
2017-05-29 13:03:41 +0100

    Added config and logging into the webserver.

--- src/tetris/server/web.clj
+++ src/tetris/server/web.clj
@@ -1,33 +1,38 @@
 (ns tetris.server.web
-  (:require [org.httpkit.server :as http]
+  (:require [taoensso.timbre :as log]
+            [org.httpkit.server :as http]
             [tetris.server.api :as api]
-            [tetris.server.mimetype :as mime]))
+            [tetris.server.mimetype :as mime]
+            [tetris.server.config :as cfg]))
 
 (def -server (atom nil))
 
-(def default-file "index.html")
-(def web-route "./target-dev/")
-
 (defn get-file [uri]
-  (try (slurp (str web-route uri))
+  (try (slurp (str (cfg/web :document-root) uri))
        (catch Exception e nil)))
 
 (defn response [uri]
   (if-let [d (get-file uri)]
-    {:status 200
-     :headers {"Content-Type" (mime/get-mime uri)}
-     :body d}
+    (do (log/info "Serving static file: " uri)
+        {:status 200
+         :headers {"Content-Type" (mime/get-mime uri)}
+         :body d})
     nil))
 
 (defn static [r]
   (if-let [f (response (:uri r))]
     f
-    (response default-file)))
+    (response (cfg/web :default-file))))
 
 (defn handler [r]
   (if-let [api-res (api/request r)]
     api-res
     (static r)))
 
-(defn start [] (reset! -server (http/run-server #'handler {:port 8081})))
-(defn stop [] (reset! -server nil))
+(defn start []
+  (log/info "Starting web server on port: " (cfg/web :port))
+  (reset! -server (http/run-server #'handler {:port (cfg/web :port)})))
+
+(defn stop []
+  (log/info "Stopping web server")
+  (reset! -server nil))
