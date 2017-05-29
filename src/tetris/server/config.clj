42deb143dbfd42b2b1faf16dff7797abd373e776
Sam Light <samlight1994@gmail.com>
2017-05-29 13:02:24 +0100

    Loading the edn config file.

--- src/tetris/server/config.clj
+++ src/tetris/server/config.clj
@@ -1,10 +1,20 @@
 (ns tetris.server.config
   (:refer-clojure :exclude [load])
-  (:require [clojure.edn :as edn]))
+  (:require [taoensso.timbre :as log]
+            [clojure.edn :as edn]
+            [clojure.java.io :as io]))
 
 (def -file-name (atom nil))
 (def -config (atom nil))
 
 (defn load [p]
+  (log/info "Loading config file " p)
   (reset! -file-name p)
-  ())
+  (reset! -config (edn/read-string (slurp p))))
+
+(defn reload [] (load @-file-name))
+
+(defn subc [& ks] (get-in @-config ks))
+
+(def web (partial subc :web))
+(def db  (partial subc :db))
