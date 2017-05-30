622402a71e5e92bbe8e99a5686564ea212654fe2
Sam Light <samlight1994@gmail.com>
2017-05-30 19:38:56 +0100

    Created the make and validate functions.

--- src/tetris/server/sign.clj
+++ src/tetris/server/sign.clj
@@ -1,3 +1,16 @@
 (ns tetris.server.sign
-  (:require [tetris.server.config :as cfg]
+  (:require [clj-time.core :as tc]
+            [tetris.server.config :as cfg]
             [buddy.sign.jwt :as jwt]))
+
+(defn- mktimeout [timeout] (tc/plus (tc/now) (tc/seconds timeout)))
+(defn- mkclaim [timeout data]
+  (reduce-kv #(assoc %1 %2 %3)
+             {:exp (mktimeout timeout)
+              :iat (tc/now)
+              :nbf (tc/plus (tc/now) (tc/seconds 3))} data))
+
+(defn make [{:keys [timeout data]}]
+  (jwt/sign (mkclaim timeout data) (cfg/sign :secret)))
+
+(defn validate [tok] (jwt/unsign tok (cfg/sign :secret)))
